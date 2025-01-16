73898896ed24fffd1379b156f6eee35ee519c35c
CellarCordial <591885295@qq.com>
2025-01-16 22:29:21 +0800

    Update

--- xmake.lua
+++ xmake.lua
@@ -77,20 +77,3 @@ target("FTS-Render")
     )
 target_end()
 
--- target("VulkanTest")
---     set_kind("binary")
---     set_languages("c99", "c++20")
---     add_defines(
---         "NDEBUG", 
---     	"DEBUG",
---         "NOMINMAX",
---         "NUM_FRAMES_IN_FLIGHT=3",
---         "CLIENT_WIDTH=1024",
---         "CLIENT_HEIGHT=768",
---         "PROJ_DIR=$(projectdir)/"
---     )
---     add_files(
--- 		"$(projectdir)/vulkan_test/*.cpp"
---     )
---     add_packages("spdlog", "glfw", "vulkansdk")
--- target_end()
