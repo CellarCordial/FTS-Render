8d9624978960586d27b11d801d3c4047d2a40d1c
CellarCordial <591885295@qq.com>
2025-01-20 16:39:34 +0800

    Update

--- source/scene/light.h
+++ source/scene/light.h
@@ -12,7 +12,17 @@ namespace fantasy
         float fall_off_start = 1.0f;
         float fall_off_end = 10.0f;
     };
-        
+
+    struct SpotLight
+    {
+        float4 color = { 1.0f, 1.0f, 1.0f, 1.0f };
+        float3 position = { 0.0f, 2.0f, 0.0f };
+        float intensity = 0.5f;
+        float inner = 0.0f;
+        float outter = 0.0f;
+
+    };
+
     struct DirectionalLight
     {
         float4 color = { 1.0f, 1.0f, 1.0f, 1.0f };
