88e2f11d0e0681e8b273655de50642dbfcb813eb
CellarCordial <591885295@qq.com>
2025-01-23 12:55:22 +0800

    Update

---
+++ source/render_pass/ddgi/probe_soft_ray_gen.h
@@ -0,0 +1,45 @@
+#ifndef RENDER_PASS_H
+#define RENDER_PASS_H
+
+#include "../../render_graph/render_pass.h"
+#include <memory>
+
+namespace fantasy
+{
+	namespace constant
+	{
+		struct Constant
+		{
+
+		};
+	}
+
+	class Pass : public RenderPassInterface
+	{
+	public:
+		Pass() { type = RenderPassType::Compute; }
+
+		bool compile(DeviceInterface* device, RenderResourceCache* cache) override;
+		bool execute(CommandListInterface* cmdlist, RenderResourceCache* cache) override;
+
+	private:
+		bool _resource_writed = false;
+		constant::Constant _pass_constant;
+
+		std::shared_ptr<BufferInterface> _buffer;
+		std::shared_ptr<TextureInterface> _texture;
+
+		std::unique_ptr<BindingLayoutInterface> _binding_layout;
+
+		std::unique_ptr<Shader> _cs;
+		std::unique_ptr<ComputePipelineInterface> _pipeline;
+
+		std::unique_ptr<BindingSetInterface> _binding_set;
+		ComputeState _compute_state;
+	};
+}
+#endif
+
+
+
+
